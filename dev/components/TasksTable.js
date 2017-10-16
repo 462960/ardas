66fe2ee86c8d73730b0de81fed1078049f86bdbb
Vladimir Maxymenko <462960@gmail.com>
2017-10-16 14:46:08 +0300

    Styled, PropTypes added. Complete.

--- dev/components/TasksTable.js
+++ dev/components/TasksTable.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import createReactClass from 'create-react-class';
 import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 export const TasksTable = (props) => {
     const {tasks} = props;
     const taskUnit = tasks.map(( x, i )=> 
 			<tr key={i} className={x.obj_status !== 'active' ? 'none' : '' }>
           		<td><Link className={x.is_high_priority ? 'priority' : ''} to={`/${x.id}`}>{x.name}</Link></td>
-          		<td>{x.tags}</td>
+          		<td><span>{x.tags ? x.tags[0] : []}<br/>{x.tags ? x.tags[1] : []}</span></td>
           		<td>{x.actual_effort}</td>
           		<td>{x.estimated_effort}</td>
           		<td>{x.due_date}</td>
            </tr>
 			)
+
     return (
     	<section>
 	<h1>Tasks list</h1>
+	<a className="github" target="_blank" href="https://github.com/462960/ardas.git">GitHub</a>
 	<div className="tbl-header">
 		<table cellPadding="0" cellSpacing="0" border="0">
 			<thead>
@@ -37,3 +40,7 @@ export const TasksTable = (props) => {
 </section>
     	)
       };
+
+      TasksTable.propTypes = {
+      	tasks: PropTypes.array.isRequired
+      }
