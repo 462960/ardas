fd175912b0a2119c54a9a2a997b5080394e821c8
Vladimir Maxymenko <462960@gmail.com>
2017-10-16 13:48:33 +0300

    Table styled

--- dev/components/TasksTable.js
+++ dev/components/TasksTable.js
@@ -2,31 +2,38 @@ import React from 'react';
 import createReactClass from 'create-react-class';
 import {Link} from 'react-router-dom';
 
-
-// export const TasksTable = createReactClass({
-// 	render(){
-// 		return <div>
-// 		<Link to="/id">TaskTable is meeeee!</Link>
-
-// 		</div>
-// 	}
-// });
-
 export const TasksTable = (props) => {
     const {tasks} = props;
-    const tasksList = tasks.map(( x, i )=> 
-    		<li key={i}>
-	        	<span><Link to={`/${x.id}`}>{x.name}</Link></span>
-	        	<span>{x.tags}</span>
-	        	<span>{x.actual_effort}</span>
-	        	<span>{x.estimated_effort}</span>
-	        	<span>{x.due_date}</span>
-	        	
-	        	<span></span>
-				
-			</li>)
-    return  <ul className="prod_holder">
-            	{tasksList}
-            </ul>
-
-            };
+    const taskUnit = tasks.map(( x, i )=> 
+			<tr key={i} className={x.obj_status !== 'active' ? 'none' : '' }>
+          		<td><Link className={x.is_high_priority ? 'priority' : ''} to={`/${x.id}`}>{x.name}</Link></td>
+          		<td>{x.tags}</td>
+          		<td>{x.actual_effort}</td>
+          		<td>{x.estimated_effort}</td>
+          		<td>{x.due_date}</td>
+           </tr>
+			)
+    return (
+    	<section>
+	<h1>Tasks list</h1>
+	<div className="tbl-header">
+		<table cellPadding="0" cellSpacing="0" border="0">
+			<thead>
+				<tr>
+					<th>Task Name</th>
+					<th>Tags</th>
+					<th>Actual effort</th>
+					<th>Estim effort</th>
+					<th>Due to</th>
+				</tr>
+			</thead>
+		</table>
+	</div>
+	<div className="tbl-content">
+		<table cellPadding="0" cellSpacing="0" border="0">
+			<tbody>{taskUnit}</tbody>
+		</table>
+	</div>
+</section>
+    	)
+      };
